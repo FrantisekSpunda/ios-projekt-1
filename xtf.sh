8b0bb2b8fe8b8434d47c95bfc2115cb81e6e2874
Frantisek Spunda <spundafr@gmail.com>
2024-03-11 18:42:23 +0100

    add arguments manipulation, list fnc

--- xtf.sh
+++ xtf.sh
@@ -1,26 +1,97 @@
 #!/bin/bash
 
-
 export POSIXLY_CORRECT=yes
 
+user=""
+command=""
+logs=()
+
+function list() {
+  echo ""
+  for log in "${logs[@]}"; do
+    awk -F ';' -v user="$user" '$1 == user' "$log"
+  done
+}
+
+function listCurrency() {
+  echo "x list currency"
+}
+
+function status() {
+  echo "x status"
+}
+
+function profit() {
+  echo "x profit"
+}
 
 function help() {
+  clear
   echo "XTF is script for showing and filtering stocks informations from log file."
   echo "Usage: xtf [-h|--help] [FILTER] [COMMAND] USERNAME [LOG FILES]"
   echo ""
   echo "List of COMMANDs:"
-  echo ""
+  echo "  list"
+  echo "  list-currency"
+  echo "  status"
+  echo "  profit"
+  echo "List of FILTERs:"
+  echo "  -a"
+  echo "  -b"
+  echo "  -c"
+  echo "Others:"
+  echo "  -h | --help"
   exit
 }
 
 function main() {
 
-  while getopts ":c:a:b:h" opt; do
-    case opt in
-      h|help) help ;;
-      c)
+  # load all arguments
+  while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+    # Filters
+    -a)
+      datetime_after="$2"
+      shift
+      ;;
+    -b)
+      datetime_before="$2"
+      shift
+      ;;
+    -c)
+      currency="$2" shift
+      ;;
+    -h | --help)
+      help
+      ;;
+    # Commands
+    list) command="list" ;;
+    list-currency) command="listCurrency" ;;
+    status) command="status" ;;
+    profit) command="profit" ;;
+    # Username and logs
+    *)
+      if [[ -z "$user" ]]; then
+        user="$1"
+      else
+        logs+=("$1")
+      fi
+      ;;
     esac
-}
+    shift
+  done
 
+  # test print of arguments and theyer load
+  echo "command: $command"
+  echo "datetime_before: $datetime_before"
+  echo "datetime_after: $datetime_after"
+  echo "currency: $currency"
+  echo "user: $user"
+  for log in "${logs[@]}"; do
+    echo "$log"
+  done
+
+  $command
+}
 
 main "$@"
