8e238db2384c2ba9aff4b87f89148dc60934fbbd
Frantisek Spunda <spundafr@gmail.com>
2024-03-17 18:14:09 +0100

    final update

--- xtf.sh
+++ xtf.sh
@@ -8,18 +8,92 @@ logs=()
 datetime_before="999999999999"
 datetime_after=""
 currency=""
-XTF_PROFIT=20
+datetime_regex='^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$'
+local_xtf_profit=20
 
-function list() {
+validate_datetime() {
+  if ! [[ "$1" =~ $datetime_regex ]]; then
+    echo "Date and time are not in right format: $1 (YYYY-MM-DD HH:MM:SS)."
+    exit
+  fi
+}
+
+function help() {
+  clear
+  echo
+  echo "XTF is script for showing and filtering stocks informations from log file."
+  echo "Usage: xtf [-h|--help] [FILTER] [COMMAND] USERNAME [LOG FILES]"
   echo ""
-  echo "list"
-  echo ""
-  for log in "${logs[@]}"; do
+  echo "List of COMMANDs:"
+  echo "  list               | list of filtered logs"
+  echo "  list-currency      | list of currencies in filtered logs"
+  echo "  status             | balance of the account grouped by currencies"
+  echo "  profit             | balance of the account with calculated income"
+  echo "List of FILTERs:"
+  echo "  -a DATETIME        | show logs only after DATETIME (must be in format YYYY-MM-DD HH:MM:SS)"
+  echo "  -b DATETIME        | show logs only before DATETIME (must be in format YYYY-MM-DD HH:MM:SS)"
+  echo "  -c CURRENCY        | show logs only with CURRENCY (must be in format YYYY-MM-DD HH:MM:SS)"
+  echo "Others:"
+  echo "  -h | --help"
+  exit
+}
+
+# load all arguments
+while [[ "$#" -gt 0 ]]; do
+  case "$1" in
+  # Filters
+  -a)
+    datetime_after="$2"
+    validate_datetime "$2"
+    shift
+    ;;
+  -b)
+    datetime_before="$2"
+    validate_datetime "$2"
+    shift
+    ;;
+  -c)
+    currency="$2" shift
+    ;;
+  -h | --help)
+    help
+    ;;
+  # Commands
+  list) command="list" ;;
+  list-currency) command="listCurrency" ;;
+  status) command="status" ;;
+  profit) command="profit" ;;
+  # Username and logs
+  *)
+    if [[ -z "$user" ]]; then
+      user="$1"
+    else
+      logs+=("$1")
+    fi
+    ;;
+  esac
+  shift
+done
+
+if [ ${#logs[@]} -eq 0 ]; then
+  echo "No input files with logs."
+  exit
+fi
+
+if ! [ -z "${XTF_PROFIT+x}" ] && [ "$XTF_PROFIT" -ge 0 ]; then
+  local_xtf_profit=$XTF_PROFIT
+fi
+
+for log in "${logs[@]}"; do
+  if [[ "$log" == *.gz ]]; then
+    zcat "$log"
+  else
     cat "$log"
-  done | awk -F ';' -v user="$user" -v datetime_before="$datetime_before" -v datetime_after="$datetime_after" -v currency="$currency" -v command="$command" -v XTF_PROFIT="$XTF_PROFIT" '
-    user == $1 && (currency == $3 || !currency) && datetime_before > $2 && datetime_after < $2 {
+  fi
+done | awk -F ';' -v user="$user" -v datetime_before="$datetime_before" -v datetime_after="$datetime_after" -v currency="$currency" -v command="$command" -v xtf_profit="$local_xtf_profit" '
+    user == $1 && (currency == $3 || !currency) && datetime_before >= $2 && datetime_after <= $2 {
       if (command == "listCurrency") { currencies[$3] = 1 }
-      else if (command == "list") { print }
+      else if (command == "list" || !command) { print }
       else if (command == "status" || command == "profit") {
         currencies[$3] = (currencies[$3] || 0) + $4
       }
@@ -32,80 +106,10 @@ function list() {
         else if (command == "status")
           print currency " : " currencies[currency]
         else if (command == "profit")
-          print currency " : " currencies[currency] * (((XTF_PROFIT || 20) + 100) / 100)
+          if(currencies[currency] > 0)
+            print currency " : " currencies[currency] * ((xtf_profit / 100) + 1)
+          else
+            print currency " : " currencies[currency] 
       }
     }
   '
-
-}
-
-function help() {
-  clear
-  echo "XTF is script for showing and filtering stocks informations from log file."
-  echo "Usage: xtf [-h|--help] [FILTER] [COMMAND] USERNAME [LOG FILES]"
-  echo ""
-  echo "List of COMMANDs:"
-  echo "  list"
-  echo "  list-currency"
-  echo "  status"
-  echo "  profit"
-  echo "List of FILTERs:"
-  echo "  -a"
-  echo "  -b"
-  echo "  -c"
-  echo "Others:"
-  echo "  -h | --help"
-  exit
-}
-
-function main() {
-
-  # load all arguments
-  while [[ "$#" -gt 0 ]]; do
-    case "$1" in
-    # Filters
-    -a)
-      datetime_after="$2"
-      shift
-      ;;
-    -b)
-      datetime_before="$2"
-      shift
-      ;;
-    -c)
-      currency="$2" shift
-      ;;
-    -h | --help)
-      help
-      ;;
-    # Commands
-    list) command="list" ;;
-    list-currency) command="listCurrency" ;;
-    status) command="status" ;;
-    profit) command="profit" ;;
-    # Username and logs
-    *)
-      if [[ -z "$user" ]]; then
-        user="$1"
-      else
-        logs+=("$1")
-      fi
-      ;;
-    esac
-    shift
-  done
-
-  # test print of arguments and theyer load
-  echo "command: $command"
-  echo "datetime_before: $datetime_before"
-  echo "datetime_after: $datetime_after"
-  echo "currency: $currency"
-  echo "user: $user"
-  for log in "${logs[@]}"; do
-    echo "$log"
-  done
-
-  list
-}
-
-main "$@"
